deac28db5e3161790b67ac4c3170721af00f0d21
Denis <den14-dn@yandex.ru>
2023-04-03 00:18:36 +0300

    Инкремент 7

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,10 @@
 package config
 
-import "github.com/caarlos0/env/v7"
+import (
+	"flag"
+
+	"github.com/caarlos0/env/v7"
+)
 
 type Config struct {
 	ServerAddress   string `env:"SERVER_ADDRESS"`
@@ -13,8 +17,16 @@ func NewConfig() (*Config, error) {
 		ServerAddress: "localhost:8080",
 		BaseURL:       "http://localhost:8080",
 	}
+	setConfigWithArgs(&cfg)
 	if err := env.Parse(&cfg); err != nil {
 		return nil, err
 	}
 	return &cfg, nil
+}
+
+func setConfigWithArgs(cfg *Config) {
+	flag.StringVar(&cfg.ServerAddress, "a", cfg.ServerAddress, "server address")
+	flag.StringVar(&cfg.BaseURL, "b", cfg.BaseURL, "base URL")
+	flag.StringVar(&cfg.FileStoragePath, "f", cfg.FileStoragePath, "file storage path")
+	flag.Parse()
 }
