7017f5645a311f76d3937cb0a0b5ec1aa086ab6a
Denis <den14-dn@yandex.ru>
2023-10-04 16:41:28 +0300

    iter17

--- internal/usecase/usecase_test.go
+++ internal/usecase/usecase_test.go
@@ -113,29 +113,6 @@ func BenchmarkExecDeleting(b *testing.B) {
 	}
 }
 
-func ExampleManager_ExecDeleting() {
-	userID := "Aa135798642"
-	items := []string{"star.example.ru/questions/20467179"}
-	fullURL := items[0]
-
-	store := storage.NewMemStorage()
-	baseURL := "http://localhost:8080"
-	manager := usecase.New(store, baseURL)
-
-	id, err := shortener.ShortenURL(fullURL)
-	if err != nil {
-		return
-	}
-	shortURL := fmt.Sprintf("%s/%s", baseURL, id)
-
-	err = store.Add(context.Background(), userID, shortURL, fullURL)
-	if err != nil {
-		return
-	}
-
-	manager.ExecDeleting(items, userID)
-}
-
 func generateSecureToken(length int) string {
 	b := make([]byte, length)
 	if _, err := rand.Read(b); err != nil {
