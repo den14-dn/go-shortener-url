bbc3d3d6a8c20a3e39fe430e24cdbb48c50357d0
Denis <den14-dn@yandex.ru>
2023-10-08 17:47:30 +0300

    iter17

--- internal/controller/doc.go
+++
@@ -1,3 +0,0 @@
-// Package controller is designed to configure the router.
-// It is also intended to describe API method handlers.
-package controller
--- internal/storage/doc.go
+++
@@ -1,6 +0,0 @@
-// Package storage is intended to initialize the data store.
-// The package contains tools for working with the PostgresQL database.
-// The package can also store data in a file on disk if the database is unavailable.
-// In cases where the database connection parameters or file location are not specified,
-// the service can store data in memory.
-package storage
